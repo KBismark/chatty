147c6d0e3d66606e44aa0b140c27d91bfd74cc01
KBismark <bismark.yamoah@amalitech.com>
2024-12-24 21:57:49 +0000

    Add chat row component

---
+++ components/chatrow.tsx
@@ -0,0 +1,149 @@
+import { ContextId, useStateStore } from "statestorejs";
+import * as Animatable from 'react-native-animatable';
+import { Pressable, StyleSheet, View } from "react-native";
+import { Text, useTheme } from "@/constants/Theme";
+import { User } from "@/stores/types";
+import { SCREEN_WIDTH } from "@/constants/Screen";
+import { ProfileHeadSizeWithMargin, StatusHead } from "./status";
+import { memo, useCallback, useMemo, useState } from "react";
+import { Tabs, useGlobalTabStore } from "@/stores/tabs";
+import { GradientBackground } from "./commons/gradient";
+import { AppStoryRing} from "./commons/rings";
+
+
+const widthWithNoHead = SCREEN_WIDTH-ProfileHeadSizeWithMargin
+const messageSliceSpace = widthWithNoHead - 60;
+
+const rowEntryAnimation = {
+    from: {transform: [{translateY: -20}]},
+    to: {transform: [{translateY: 0}]},
+}
+
+export const Row = memo(({propsSource, ring}: {propsSource:string|ContextId, ring?: boolean})=>{
+    const {colors, mode} =  useTheme();
+    const {white, divider, black, highlights} = colors;
+    let {name, contact, last, isUser} = useStateStore<User>('users', propsSource as string)||{}
+    const [pressing, setPressing] = useState(false);
+    const {activeTab} = useGlobalTabStore({watch: ['activeTab']})
+    const hideMessageIndicators = (['search'] as Tabs[]).includes(activeTab)||!isUser||!last.messagePreview;
+    
+    let nameUsed = name;
+    [name, nameUsed] = useMemo(()=>{
+        name = name||contact||'Unknown';
+        nameUsed = name.trim().slice(0,20);
+        nameUsed = nameUsed.length>20?`${nameUsed}...`:nameUsed;
+        return [name, nameUsed]
+    },[name])
+
+    let previewedMessage = useMemo(()=>{
+        let previewedMessage = !isUser?
+        `Invite ${name} to start conversation.`:
+        last.messagePreview? last.messagePreview:
+        `Send a message to ${name} to start a conversation.`;
+        
+        previewedMessage = previewedMessage.trim();
+        
+        return previewedMessage.length>=32?`${previewedMessage.slice(0,32).trim()}...`: previewedMessage;
+    },[isUser, last.messagePreview?.slice(0,50)])
+
+    const innerStyles = useMemo(()=>{
+        return {
+            container: {backgroundColor: white,},
+            nameText: {fontWeight: '700', fontSize: 16, color: black},
+        }
+    },[mode])
+
+    const pressableStyle = useMemo(()=>{
+        return [styles.displayRow, styles.row, {backgroundColor: pressing? highlights: white, borderTopColor: divider,}]
+    },[mode, pressing])
+
+    const onPressed = ()=>{
+        // setPressing(false)
+    }
+    const onPressedIn = useCallback(()=>{
+        setPressing(true)
+    },[pressing])
+    const onPressedOut = useCallback(()=>{
+        setPressing(false)
+    },[pressing])
+    
+    
+
+    return (
+        <Animatable.View animation={rowEntryAnimation} duration={500} style={innerStyles.container}>
+            <Pressable onPress={onPressed} onPressIn={onPressedIn} onPressOut={onPressedOut}  style={pressableStyle}>
+                {
+                    ring?
+                    <AppStoryRing segments={4} strokeWidth={0} style={{marginHorizontal: 8}} size={60}>
+                        <StatusHead stripMargin={true} propsSource={propsSource} size={54} />
+                    </AppStoryRing>
+                    :
+                    <StatusHead propsSource={propsSource} size={60} />
+                }
+                <View style={styles.messageNoHead}>
+                    <View style={styles.displayRow}>
+                        <Text style={innerStyles.nameText as any}>{nameUsed}</Text>
+                        {!hideMessageIndicators&&<Text style={styles.messageDate}>{last.date||'01/24'}</Text>}
+                    </View>
+                    <View style={styles.displayRow}>
+                        <Text style={styles.messageSlice}>
+                            {previewedMessage}
+                        </Text>
+                        <View>
+                            {!hideMessageIndicators&&<MessageIndicator propsSource="" />}
+                        </View>
+                    </View>
+                </View>
+            </Pressable>
+        </Animatable.View>
+    )
+})
+
+const getRandomMessageIndicatorNumber = ()=> (Math.floor(Math.random() * 1000) %  99) + 1;
+
+const MessageIndicator = memo(({propsSource}: {propsSource: string})=>{
+    const {primary} = useTheme().colors;
+    const number = getRandomMessageIndicatorNumber();
+    const paddings = {paddingVertical: 5, paddingHorizontal: number<10?9:6}
+    return (
+        <GradientBackground style={{backgroundColor: primary, borderRadius: 15,}}>
+            <View style={styles.messageIndicator}>
+                <Text style={[styles.indicatorText, paddings]}>{number}</Text>
+            </View>
+        </GradientBackground>
+    )
+})
+
+
+const styles = StyleSheet.create({
+    round: {
+        borderRadius: 9999,
+        width: 60, height: 60,
+        marginHorizontal: 15
+    },
+    displayRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    row:{
+        borderTopWidth: 1,
+        paddingVertical: 8,
+    },
+    messageIndicator: {
+        borderWidth: 0,
+        borderColor: 'rgba(0,0,0,0)',
+        borderRadius: 15, 
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    indicatorText: {color: '#ffffff', fontSize: 12, fontWeight: 500, margin: 0,  },
+    messageSlice: {fontSize: 13,marginRight: 5, maxWidth: messageSliceSpace},
+    messageDate: {fontSize: 13},    
+    messageNoHead: {width: widthWithNoHead, paddingRight: 10},
+    messageChevron: {marginLeft: 15},
+
+
+})
+
+
