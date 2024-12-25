d9f5d8a287584291f9d039bdc823e2f999501788
KBismark <bismarkkwabenayamoah@gmail.com>
2024-12-25 17:04:53 +0000

    Make reactions reactive

--- components/feeditem.tsx
+++ components/feeditem.tsx
@@ -1,6 +1,6 @@
 import { Text, useTheme } from "@/theme/Theme";
 import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
-import { memo, useMemo} from "react";
+import { memo, useCallback, useMemo, useState} from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { StatusHead } from "./status";
 import { GradientBackground } from "./commons/gradient";
@@ -40,25 +40,43 @@ export const Item = memo(({propsSource:userId}: {propsSource: string})=>{
             <GradientBackground colors={generateGradientColors()} style={{...styles.displayRow, height: 250, margin: 5, borderRadius: 20, marginBottom: 5, justifyContent: 'center'}}>
                 {
                     /* Post space */ 
-                    <Text style={{color: '#ffffff', fontWeight: 700, paddingHorizontal: 25, textAlign:'center', fontSize: 20}}>Some cheerful message from {firstName}!</Text>
+                    <Text style={{color: '#ffffff', fontWeight: 700, paddingHorizontal: 25, textAlign:'center', fontSize: 25}}>Some cheerful message from {firstName}!</Text>
                 }
             </GradientBackground>
             <View style={[styles.displayRow, { marginVertical: 10, marginLeft: 10}]}>
-                
-                <View style={[styles.displayRow]}>
-                    <TouchableOpacity style={[styles.displayRow, styles.reactionIcons, {backgroundColor: highlights, borderColor: divider, marginLeft: 0}]}>
-                        <Feather name='bookmark' size={22} color={colors.primary} />
-                    </TouchableOpacity>
-                    <View style={styles.displayRow}>
-                        <TouchableOpacity style={[styles.displayRow, styles.reactionIcons, {backgroundColor: highlights, borderColor: divider}]}>
-                            <MaterialCommunityIcons name='heart-outline' size={22} color={colors.danger} />
-                        </TouchableOpacity>
-                        <Text style={{marginLeft: 5, fontSize: 13, fontWeight: '500'}}> 2k+ </Text>
-                    </View>
-                </View>
+                <Reactions userId={userId} postId="" />
                 <TouchableOpacity style={[styles.displayRow, styles.reactionIcons, {backgroundColor: highlights, borderColor: divider}]}>
                     <Feather name='more-horizontal' size={22} color={fadedBlack} />
                 </TouchableOpacity>
+            </View>
+        </View>
+    )
+})
+
+
+const Reactions = memo(({userId, postId}:{userId: string; postId: string})=>{
+    const {colors} = useTheme()
+    const [bookmarked, setBookmark] = useState(false)
+    const [liked, setFavourite] = useState(false)
+
+    const onBookmark = useCallback(()=>{
+        setBookmark(!bookmarked)
+    },[bookmarked])
+
+    const onLike = useCallback(()=>{
+        setFavourite(!liked)
+    },[liked])
+
+    return (
+        <View style={[styles.displayRow]}>
+            <TouchableOpacity onPress={onBookmark} style={[styles.displayRow, styles.reactionIcons, {backgroundColor: colors.highlights, borderColor: colors.divider, marginLeft: 0}]}>
+                <MaterialCommunityIcons name={bookmarked?'bookmark':'bookmark-outline'} size={22} color={colors.primary} />
+            </TouchableOpacity>
+            <View style={styles.displayRow}>
+                <TouchableOpacity onPress={onLike} style={[styles.displayRow, styles.reactionIcons, {backgroundColor: colors.highlights, borderColor: colors.divider}]}>
+                    <MaterialCommunityIcons name={liked?'heart':'heart-outline'} size={22} color={colors.danger} />
+                </TouchableOpacity>
+                <Text style={{marginLeft: 5, fontSize: 13, fontWeight: '500'}}> 2k+ </Text>
             </View>
         </View>
     )
