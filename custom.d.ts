9ab5f6637e39784ae9d658066e2628192fee5aea
KBismark <bismarkkwabenayamoah@gmail.com>
2024-12-25 01:08:03 +0000

    Add configuration files

---
+++ custom.d.ts
@@ -0,0 +1,12 @@
+declare module '*.jpg'{
+    const src = '';
+    export default src
+}
+declare module '*.jpeg'{
+    const src = '';
+    export default src
+}
+declare module '*.png'{
+    const src = '';
+    export default src
+}
